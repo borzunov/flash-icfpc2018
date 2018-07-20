1bf714d8bc883fe039b00047bbad47e6aac83b39
borzov <borzov@skbkontur.ru>
2018-07-20 18:47:20 +0500

    add sandbox project with basics mongo examples

---
+++ Sandbox/EntryPoint.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sandbox
+{
+    public class EntryPoint
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Sandbox code here!");
+        }
+    }
+}
---
+++ Sandbox/Mongo.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using NUnit.Framework;
+
+namespace Sandbox
+{
+    public class Lol
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        [BsonElement("name")]
+        public string Name { get; set; }
+
+        [BsonElement("lols_numbers")]
+        public List<int> LolsNumbers { get; set; }
+    }
+
+    [Explicit("It's examples")]
+    public class Mongo
+    {
+        private IMongoDatabase db;
+
+        [OneTimeSetUp]
+        public void CreateDatabaseConnection()
+        {
+            const string connectionString = "mongodb://admin:sw8k83ng01bw5@vm-dev-cont2:27017";
+            var client = new MongoClient(connectionString);
+
+            db = client.GetDatabase("my_lols");
+        }
+
+        [Test]
+        public async Task CreateLols()
+        {
+            db.CreateCollection("big_lols");
+
+            var lols = db.GetCollection<Lol>("big_lols");
+
+            await lols.InsertManyAsync(new[]
+            {
+                new Lol
+                {
+                    Name = "Best lol",
+                    LolsNumbers = new List<int>() {1, 2, 3}
+                },
+                new Lol
+                {
+                    Name = "Not lol",
+                    LolsNumbers = new List<int>() {5, 6, 7, 8}
+                },
+                new Lol
+                {
+                    Name = "Lol^2",
+                    LolsNumbers = new List<int>() {9, 10, 11, 12, 13}
+                }
+            });
+        }
+
+        [Test]
+        public async Task SearchLols()
+        {
+            var collection = db.GetCollection<Lol>("big_lols");
+
+            var filter = new BsonDocument("$or", new BsonArray{
+                new BsonDocument("name", "Best lol"),
+                new BsonDocument("lols_numbers", new BsonDocument("$size", 5))
+            });
+            var bestLols = await collection.FindAsync(filter);
+
+            var lols = await bestLols.ToListAsync();
+
+            foreach (var lol in lols)
+            {
+                Console.WriteLine($"Id = {lol.Id}\nName = {lol.Name}\n");
+            }
+        }
+
+        [Test]
+        public void DropLols()
+        {
+            db.DropCollection("big_lols");
+        }
+    }
+}
