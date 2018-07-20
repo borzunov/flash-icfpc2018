151543c68d43180afa963adf71b8321ac617fa10
borzov <borzov@skbkontur.ru>
2018-07-20 23:48:51 +0500

    add Vector

---
+++ src/Flash/Infrastructure/Vector.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Flash.Infrastructure
+{
+    public class Vector
+    {
+        public readonly int X;
+        public readonly int Y;
+        public readonly int Z;
+
+        public Vector(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        /// <summary>
+        /// Manhattan length
+        /// </summary>
+        public int Mlen => Math.Abs(X) + Math.Abs(Y) + Math.Abs(Z);
+
+        /// <summary>
+        /// Chessboard length
+        /// </summary>
+        public int Clen => Math.Max(Math.Max(Math.Abs(X), Math.Abs(Y)), Math.Abs(Z));
+
+        public static Vector operator +(Vector v1, Vector v2) => new Vector(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
+        public static Vector operator -(Vector v1, Vector v2) => new Vector(v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z);
+        public static Vector operator -(Vector v) => new Vector(-v.X, -v.Y, -v.Z);
+
+        /// <summary>
+        /// (this - other).Mlen == 1;
+        /// </summary>
+        public bool IsAdjacent(Vector other)
+        {
+            return (this - other).Mlen == 1;
+        }
+
+        #region equality members
+        protected bool Equals(Vector other)
+        {
+            return X == other.X && Y == other.Y && Z == other.Z;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+                return false;
+            if (ReferenceEquals(this, obj))
+                return true;
+            if (obj.GetType() != this.GetType())
+                return false;
+            return Equals((Vector) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = X;
+                hashCode = (hashCode*397) ^ Y;
+                hashCode = (hashCode*397) ^ Z;
+                return hashCode;
+            }
+        }
+        #endregion
+    }
+}
---
+++ tests/UnitTests/VectorTests.cs
@@ -0,0 +1,80 @@
+﻿using Flash.Infrastructure;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    public class VectorTests
+    {
+        [TestCase(1, 2, 3, ExpectedResult = 3)]
+        [TestCase(3, 1, 2, ExpectedResult = 3)]
+        [TestCase(3, 2, 1, ExpectedResult = 3)]
+        [TestCase(-2, 1, 0, ExpectedResult = 2)]
+        [TestCase(-2, -3, 1, ExpectedResult = 3)]
+        [TestCase(0, 0, 1, ExpectedResult = 1)]
+        [TestCase(0, 1, 0, ExpectedResult = 1)]
+        [TestCase(1, 0, 0, ExpectedResult = 1)]
+        public int Clen_Correct(int x, int y, int z)
+        {
+            return new Vector(x, y, z).Clen;
+        }
+
+        [TestCase(1, 1, 1, ExpectedResult = 3)]
+        [TestCase(-1, -1, -1, ExpectedResult = 3)]
+        [TestCase(0, 0, 0, ExpectedResult = 0)]
+        [TestCase(0, 0, 1, ExpectedResult = 1)]
+        [TestCase(0, 1, 0, ExpectedResult = 1)]
+        [TestCase(1, 0, 0, ExpectedResult = 1)]
+        [TestCase(1, 2, -3, ExpectedResult = 6)]
+        public int Mlen_Correct(int x, int y, int z)
+        {
+            return new Vector(x, y, z).Mlen;
+        }
+
+        public static TestCaseData[] SumCases = 
+        {
+            new TestCaseData(new Vector(1, 1, 1), new Vector(1, 1, 1), new Vector(2, 2, 2)),
+            new TestCaseData(new Vector(1, 2, 3), new Vector(-1, -2, -3), new Vector(0, 0, 0)),
+            new TestCaseData(new Vector(0, 0, 1), new Vector(1, 0, 0), new Vector(1, 0, 1)),
+        };
+
+        [TestCaseSource(nameof(SumCases))]
+        public void Sum_Correct(Vector v1, Vector v2, Vector result)
+        {
+            (v1 + v2).Should().Be(result);
+        }
+
+
+
+        public static TestCaseData[] DiffCases = 
+        {
+            new TestCaseData(new Vector(1, 1, 1), new Vector(1, 1, 1), new Vector(0, 0, 0)),
+            new TestCaseData(new Vector(1, 2, 3), new Vector(-1, -2, -3), new Vector(2, 4, 6)),
+            new TestCaseData(new Vector(0, 0, 1), new Vector(1, 0, 0), new Vector(-1, 0, 1)),
+        };
+
+        [TestCaseSource(nameof(DiffCases))]
+        public void Diff_Correct(Vector v1, Vector v2, Vector result)
+        {
+            (v1 - v2).Should().Be(result);
+        }
+
+        public static TestCaseData[] AdjacentCases = 
+        {
+            new TestCaseData(new Vector(0, 0, 0), new Vector(0, 0, 1), true),
+            new TestCaseData(new Vector(0, 0, 0), new Vector(0, 0, 0), false),
+            new TestCaseData(new Vector(0, 0, 0), new Vector(10, 1, 0), false),
+            new TestCaseData(new Vector(0, 0, 0), new Vector(1, 1, 0), false),
+            new TestCaseData(new Vector(0, 0, 0), new Vector(1, 1, 1), false),
+            new TestCaseData(new Vector(1, 2, 3), new Vector(1, 1, 3), true),
+            new TestCaseData(new Vector(1, 2, 3), new Vector(1, 2, 1), false),
+            new TestCaseData(new Vector(-1, -2, -3), new Vector(-1, -2, -4), true)
+        };
+
+        [TestCaseSource(nameof(AdjacentCases))]
+        public void Adjacent_Correct(Vector v1, Vector v2, bool result)
+        {
+            v1.IsAdjacent(v2).Should().Be(result);
+        }
+    }
+}
