0c0ef40a132e5c89bc614bb9dffd525c45efa9ec
borzov <borzov@skbkontur.ru>
2018-07-21 05:37:22 +0500

    AI and models

---
+++ src/Flash/Infrastructure/AI/FileAI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Windows.Input;
+
+namespace Flash.Infrastructure
+{
+    public class FileAI : IAI
+    {
+        private readonly string filePath;
+
+        public FileAI(string filePath)
+        {
+            this.filePath = filePath; //TODO Add deserializer
+        }
+
+        public IEnumerable<ICommand> NextStep(State state)
+        {
+            return new List<ICommand>();
+        }
+    }
+}
---
+++ src/Flash/Infrastructure/AI/IAI.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Windows.Input;
+
+namespace Flash.Infrastructure
+{
+    public interface IAI
+    {
+        IEnumerable<ICommand> NextStep(State state);
+    }
+}
