4a201b0d2065aff15069cc4dc764ced773494745
Starcev Michail <starcev.m@skbkontur.ru>
2018-07-21 18:36:22 +0500

    cleanup

---
+++ src/evaller/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evaller
+{
+	class Program
+	{
+		public class AIResult
+		{
+			public long energy;
+			public byte[] data;
+		}
+
+		static void Main(string[] args)
+		{
+			var resourcePath = "C:\\Users\\starcev.m\\Desktop\\result";
+			var outPath = "C:\\Users\\starcev.m\\Desktop\\result";
+			List<Func<string,AIResult>> listOfAI = new List<Func<string, AIResult>>();
+
+			foreach (var file in Directory.EnumerateFiles(outPath))
+			{
+				var resultsForFile = new List<AIResult>();
+				foreach (var ai in listOfAI)
+				{
+					resultsForFile.Add(ai(file));
+				}
+
+				var best = resultsForFile.OrderBy(x => x.energy).First();
+				File.WriteAllBytes(
+					$"c:\\users\\starcev.m\\desktop\\result\\{Path.GetFileName(file).Substring(0, 5)}.nbt",best.data);
+			}
+
+		}
+	}
+}
