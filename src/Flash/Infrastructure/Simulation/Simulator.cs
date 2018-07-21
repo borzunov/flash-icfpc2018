5aadd75c45f531782191aa00e9103399c7cc504b
borzov <borzov@skbkontur.ru>
2018-07-21 08:12:46 +0500

    add validator

--- src/Flash/Infrastructure/Simulation/Simulator.cs
+++ src/Flash/Infrastructure/Simulation/Simulator.cs
@@ -81,7 +81,71 @@ namespace Flash.Infrastructure.Simulation
                         break;
                 }
             }
-            //throw new NotImplementedException();
+
+            for (var i = 0; i < regions.Count-1; i++)
+            {
+                for (var j = i+1; j < regions.Count; j++)
+                {
+                    if (Intersects(regions[i], regions[j]))
+                    {
+                        throw new Exception("Commands used regionst itersects");
+                    }
+                }
+            }
+        }
+
+        private static bool Intersects(Region r1, Region r2)
+        {
+            int x1, y1, x2, y2, x3, y3, x4, y4;
+            if (r1.Min.X == r1.Max.X && r1.Max.X == r2.Min.X && r2.Min.X == r2.Max.X)
+            {
+                x1 = r1.Min.Y;
+                y1 = r1.Min.Z;
+
+                x2 = r1.Max.Y;
+                y2 = r1.Max.Z;
+
+                x3 = r2.Min.Y;
+                y3 = r2.Min.Z;
+
+                x4 = r2.Max.Y;
+                y4 = r2.Max.Z;
+            } 
+            else if (r1.Min.Y == r1.Max.Y && r1.Max.Y == r2.Min.Y && r2.Min.Y == r2.Max.Y)
+            {
+                x1 = r1.Min.X;
+                y1 = r1.Min.Z;
+                
+                x2 = r1.Max.X;
+                y2 = r1.Max.Z;
+                
+                x3 = r2.Min.X;
+                y3 = r2.Min.Z;
+                
+                x4 = r2.Max.X;
+                y4 = r2.Max.Z;
+            }
+            else if (r1.Min.Z == r1.Max.Z && r1.Max.Z == r2.Min.Z && r2.Min.Z == r2.Max.Z)
+            {
+                x1 = r1.Min.X;
+                y1 = r1.Min.Y;
+
+                x2 = r1.Max.X;
+                y2 = r1.Max.Y;
+
+                x3 = r2.Min.X;
+                y3 = r2.Min.Y;
+
+                x4 = r2.Max.X;
+                y4 = r2.Max.Y;
+            }
+            else
+            {
+                return false;
+            }
+
+            return x1 <= x3 && x3 <= x2 && x1 <= x4 && x4 <= x2 &&
+                   y3 <= y2 && y2 <= y4 && y3 <= y1 && y1 <= y4;
         }
     }
 }
