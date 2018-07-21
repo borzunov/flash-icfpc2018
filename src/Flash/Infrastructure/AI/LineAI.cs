cdd9c65ecd7f63ac4d34ef45e1da1ea46219c053
rkhapov <r.khapov@yandex.ru>
2018-07-22 00:54:15 +0500

    wip

---
+++ src/Flash/Infrastructure/AI/LineAI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Flash.Infrastructure.AI;
+using Flash.Infrastructure.Commands;
+using Flash.Infrastructure.Models;
+
+namespace Flash.Infrastructure
+{
+    public class LineAI : IAI
+    {
+        private readonly Matrix modelToDraw;
+
+        public LineAI(Matrix modelToDraw)
+        {
+            this.modelToDraw = modelToDraw;
+        }
+
+        public IEnumerable<ICommand> NextStep(State state)
+        {
+            
+        }
+    }
+}
