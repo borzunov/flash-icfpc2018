5cd338a99a614b28de69cc2877699219f0e148ba
Valeriy Kuzmin <malcoriel@gmail.com>
2018-07-21 07:29:05 +0500

    refactor: completely refactor control panel

--- src/Visualize/src/containers/home/ControlPanel.js
+++ src/Visualize/src/containers/home/ControlPanel.js
@@ -1,39 +1,128 @@
 import case1 from '../../test-logs/1'
 import React from 'react'
+import store from '../../store'
+import { withProps, withHandlers } from 'recompose'
+import { compose } from 'redux'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { changeBot, changeSize, changeVoxel } from '../../modules/space'
+import Vector from '../../modules/Vector'
+import Queue from 'async/queue'
+import { LogAction } from '../../test-logs/LogAction'
 
-export default function ControlPanel({ changeSize, mapSize, fillRandomVoxel, addRandomBot, doPlayLog, doReset, enqueue }) {
-  return <div className="control-panel">
-      <h3>Debug controls</h3>
-      <button onClick={() => {
-        return changeSize(Math.min(mapSize + 10, 250))
-      }}>size + 10
-      </button>
-      <button onClick={() => {
-        return changeSize(Math.max(10, mapSize - 10))
-      }}>size - 10
-      </button>
-      <button onClick={() => {
-        doReset()
-      }}>reset
-      </button>
-      <button onClick={fillRandomVoxel}>fill random voxel
-      </button>
-      <button onClick={() => {
-        for (let i = 0; i < mapSize * mapSize * mapSize / 8; ++i) {
-          enqueue(() => fillRandomVoxel())
-        }
-      }}>fill 1/8
-      </button>
-      <button onClick={() => {
-        for (let i = 0; i < 10; ++i) {
-          enqueue(() => addRandomBot())
-        }
-      }}>add some bots
-      </button>
-      <button onClick={() => {
-        doReset()
-        doPlayLog(case1)
-      }}>play test 1
-      </button>
-    </div>
+let currentWait = 10
+const syncQueueWithWait = new Queue((f, callback) => {
+  f()
+  setTimeout(callback, currentWait)
+}, 1)
+
+const SPEED_CONST = 4
+
+const reset = () => {
+  // clear queue to avoid more actions that are not finished
+  syncQueueWithWait.pause()
+  syncQueueWithWait.remove(() => true)
+  syncQueueWithWait.resume()
+  // reset dev-tools
+  store.liftedStore.dispatch({ type: 'RESET' })
+  // reset our state
+  store.dispatch({ type: 'RESET' })
 }
+
+const playLog = ({ changeSize, changeVoxel, changeBot }, { size, log }) => {
+  changeSize(size)
+  for (let act of log) {
+    switch (act.t) {
+      case LogAction.Add:
+        changeBot(act.p, true)
+        break
+      case LogAction.Remove:
+        changeBot(act.p, false)
+        break
+      case LogAction.Fill:
+        changeVoxel(act.p, true)
+        break
+      default:
+        // do nothing
+        break
+    }
+  }
+}
+
+function ControlPanelImpl({ changeSize, mapSize, fillRandomVoxel, addRandomBot, doPlayLog, enqueue }) {
+  currentWait = mapSize / SPEED_CONST
+  return <div className="control-panel">
+    <h3>Debug controls</h3>
+    <button onClick={() => {
+      return changeSize(Math.min(mapSize + 10, 250))
+    }}>size + 10
+    </button>
+    <button onClick={() => {
+      return changeSize(Math.max(10, mapSize - 10))
+    }}>size - 10
+    </button>
+    <button onClick={() => {
+      reset()
+    }}>reset
+    </button>
+    <button onClick={fillRandomVoxel}>fill random voxel
+    </button>
+    <button onClick={() => {
+      for (let i = 0; i < mapSize * mapSize * mapSize / 8; ++i) {
+        enqueue(() => fillRandomVoxel())
+      }
+    }}>fill 1/8
+    </button>
+    <button onClick={() => {
+      for (let i = 0; i < 10; ++i) {
+        enqueue(() => addRandomBot())
+      }
+    }}>add some bots
+    </button>
+    <button onClick={() => {
+      reset()
+      doPlayLog(case1)
+    }}>play test 1
+    </button>
+  </div>
+}
+
+export default compose(
+  withProps(store),
+  connect(({ space }) => {
+      return {
+        mapSize: space.size
+      }
+    },
+    dispatch =>
+      bindActionCreators(
+        {
+          changeSize,
+          changeVoxel,
+          changeBot
+        },
+        dispatch
+      )
+  ),
+  withHandlers({
+      makeRand: () => (max) => {
+        return Number(parseInt(String(Math.random() * max % max), 10))
+      },
+      enqueue: () => (task, cb) => syncQueueWithWait.push(task, cb),
+    }
+  ),
+  withHandlers({
+    doPlayLog: ({changeSize, changeVoxel, changeBot}) => ({size, log}) => {
+      playLog({ changeSize, changeVoxel, changeBot }, { size, log });
+    },
+    fillRandomVoxel: ({ mapSize, makeRand, changeVoxel }) => () => {
+      const rand = Vector(makeRand(mapSize), makeRand(mapSize), makeRand(mapSize))
+      return changeVoxel(rand, true)
+    },
+
+    addRandomBot: ({ mapSize, makeRand, changeBot }) => () => {
+      const rand = Vector(makeRand(mapSize), makeRand(mapSize), makeRand(mapSize))
+      return changeBot(rand, true)
+    }
+  })
+)(ControlPanelImpl)
--- src/Visualize/src/containers/home/ThreeScene.js
+++ src/Visualize/src/containers/home/ThreeScene.js
@@ -2,59 +2,18 @@ import React from 'react'
 import * as THREE from 'three'
 import React3 from 'react-three-renderer'
 import Vector from '../../modules/Vector'
-import store from '../../store'
 import BotContainer from './BotContainer'
 import { vecToThree } from './coords'
 import { withSize } from 'react-sizeme'
 import CoordinatesHelpers from './CoordinatesHelpers'
 import ControlPanel from './ControlPanel'
-import { LogAction } from '../../test-logs/LogAction'
 import FillContainer from './FillContainer'
-import Queue from 'async/queue'
 import { slide as Menu } from 'react-burger-menu'
 import HelpText from './HelpText'
 
-let currentWait = 10
-const syncQueueWithWait = new Queue((f, callback) => {
-  f()
-  setTimeout(callback, currentWait)
-}, 1)
-
-const SPEED_CONST = 4
-
-const reset = () => {
-  // clear queue to avoid more actions that are not finished
-  syncQueueWithWait.pause()
-  syncQueueWithWait.remove(() => true)
-  syncQueueWithWait.resume()
-  // reset dev-tools
-  store.liftedStore.dispatch({ type: 'RESET' })
-  // reset our state
-  store.dispatch({ type: 'RESET' })
-}
 
 // noinspection JSUnresolvedFunction
 const OrbitControls = require('three-orbit-controls')(THREE)
-
-const playLog = ({ changeSize, changeVoxel, changeBot }, { size, log }) => {
-  changeSize(size)
-  for (let act of log) {
-    switch (act.t) {
-      case LogAction.Add:
-        changeBot(act.p, true)
-        break
-      case LogAction.Remove:
-        changeBot(act.p, false)
-        break
-      case LogAction.Fill:
-        changeVoxel(act.p, true)
-        break
-      default:
-        // do nothing
-        break
-    }
-  }
-}
 
 class ThreeScene extends React.PureComponent {
   componentDidMount() {
@@ -66,25 +25,8 @@ class ThreeScene extends React.PureComponent {
     this.controls = null
   }
 
-  makeRand = (max) => {
-    return Number(parseInt(String(Math.random() * max % max), 10))
-  }
-
-  fillRandomVoxel = () => {
-    const { mapSize } = this.props
-    const rand = Vector(this.makeRand(mapSize), this.makeRand(mapSize), this.makeRand(mapSize))
-    return this.props.changeVoxel(rand, true)
-  }
-
-  addRandomBot = () => {
-    const { mapSize } = this.props
-    const rand = Vector(this.makeRand(mapSize), this.makeRand(mapSize), this.makeRand(mapSize))
-    return this.props.changeBot(rand, true)
-  }
-
   render() {
-    const { mapSize, changeSize } = this.props
-    currentWait = mapSize / SPEED_CONST
+    const { mapSize } = this.props
     const { width, height } = this.props.size
 
     const smallBoxSize = new THREE.Vector3(1, 1, 1)
@@ -93,19 +35,7 @@ class ThreeScene extends React.PureComponent {
 
     return (<div style={{ width: '100%', height: '100%' }}>
       <Menu isOpen={true} noOverlay closeButton={false}>
-        <ControlPanel
-          {...
-            {
-              changeSize,
-              mapSize,
-              fillRandomVoxel: this.fillRandomVoxel,
-              addRandomBot: this.addRandomBot,
-              doPlayLog: (testCase) => playLog(this.props, testCase),
-              doReset: reset,
-              enqueue: (task, cb) => syncQueueWithWait.push(task, cb)
-            }
-          }
-        />
+        <ControlPanel/>
       </Menu>
       <HelpText/>
       <React3
