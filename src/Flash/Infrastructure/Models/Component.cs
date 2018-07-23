c32295d76028fcaabee4ab37004450a3b1c6ee24
Alexander Borzunov <hxrussia@gmail.com>
2018-07-23 13:46:26 +0500

    Find connected components of one sign in XOR matrix

---
+++ src/Flash/Infrastructure/Models/Component.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Flash.Infrastructure.Models
+{
+    public class Component
+    {
+        public readonly List<Vector> Points;
+        public readonly HashSet<int> Neighs;
+
+        public Component(List<Vector> points, HashSet<int> neighs)
+        {
+            Points = points;
+            Neighs = neighs;
+        }
+    }
+}
